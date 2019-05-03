00f8f160bb10051f9c0a3e208b586224047d6843
dimitri <dimitri@debian>
2019-05-03 17:03:32 +0200

    Fixed iteration over beta to correct one

--- multi_run_uni_server.sh
+++ multi_run_uni_server.sh
@@ -2,7 +2,7 @@ declare -a alpha=("0.000001" "0.00001")
 declare -a beta=("0.000001" "0.00001" "0.0001" "0.001" "0.01" "0.1")
 for a in "${alpha[@]}"
 do
-    for b in "${alpha[@]}"
+    for b in "${beta[@]}"
     do
 	echo "alpha $a beta: $b"
 	python train.py --cuda --model mlp --method sgd     --save --epochs 100 --alpha_0 $a --beta  $b --silent --reduction 
