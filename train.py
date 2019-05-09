53de88c5d7c5f858678f1e5e418e9d352ebff8c5
dimitri <dimitri@debian>
2019-05-09 18:55:11 +0200

    Fixed custom cnn, now working with the whole dataset. Needs --reduction implementation

--- train.py
+++ train.py
@@ -46,67 +46,58 @@ class MLP(nn.Module):
         x = self.lin3(x)
         return x
 
-    
+
 class custom_cnn(nn.Module):
     """CNN."""
-    
+
     def __init__(self):
         """CNN Builder."""
-        super(CNN, self).__init__()
-        
+        super(custom_cnn, self).__init__()
         self.conv_layer = nn.Sequential(
-                                        
-        # Conv Layer block 1
-        nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
-        nn.BatchNorm2d(32),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=2, stride=2),
-        
-        # Conv Layer block 2
-        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-        nn.BatchNorm2d(128),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=2, stride=2),
-        nn.Dropout2d(p=0.05),
-        
-        # Conv Layer block 3
-        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-        nn.BatchNorm2d(256),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        
-        
-        self.fc_layer = nn.Sequential(
-        nn.Dropout(p=0.1),
-        nn.Linear(4096, 1024),
-        nn.ReLU(inplace=True),
-        nn.Linear(1024, 512),
-        nn.ReLU(inplace=True),
-        nn.Dropout(p=0.1),
-        nn.Linear(512, 10)
+            # Conv Layer block 1
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Conv Layer block 2
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=0.05),
+            # Conv Layer block 3
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
+        self.fc_layer = nn.Sequential(
+            nn.Dropout(p=0.1),
+            nn.Linear(4096, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.1),
+            nn.Linear(512, 10)
+        )
 
     def forward(self, x):
-    """Perform forward."""
-        
+        """Perform forward."""
         # conv layers
         x = self.conv_layer(x)
-        
         # flatten
         x = x.view(x.size(0), -1)
-        
         # fc layer
         x = self.fc_layer(x)
-        
         return x
+
 
 def train(opt, log_func=None):
     torch.manual_seed(opt.seed)
@@ -158,28 +149,21 @@ def train(opt, log_func=None):
         task = 'CIFAR10'
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
-        x = datasets.CIFAR10('./data', train=True, download=True,
-                             transform=transforms.Compose([
-                                 transforms.ToTensor(),
-                                 normalize,
-                                 transforms.RandomCrop(32, 4),
-                                 transforms.RandomHorizontalFlip()]))
-        if opt.reduction:
-            x.data = x.data[:int(len(x.data) * DATASET_REDUCTION_FACTOR)]
         train_loader = torch.utils.data.DataLoader(
-            x,
+            datasets.CIFAR10(root='./data', train=True, transform=transforms.Compose([
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomCrop(32, 4),
+                transforms.ToTensor(),
+                normalize,
+            ]), download=True),
             batch_size=opt.batchSize, shuffle=True,
             num_workers=opt.workers, pin_memory=True)
-        y = datasets.CIFAR10('./data', train=False, transform=transforms.Compose([
-                                 transforms.ToTensor(),
-                                 normalize,
-                                 transforms.RandomCrop(32, 4),
-                                 transforms.RandomHorizontalFlip()]))
-        if opt.reduction:
-            y.data = y.data[:int(len(y.data) * DATASET_REDUCTION_FACTOR)]
-            
+
         valid_loader = torch.utils.data.DataLoader(
-            y,
+            datasets.CIFAR10(root='./data', train=False, transform=transforms.Compose([
+                transforms.ToTensor(),
+                normalize,
+            ])),
             batch_size=opt.batchSize, shuffle=False,
             num_workers=opt.workers, pin_memory=True)
     else:
